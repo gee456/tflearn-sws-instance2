c7db3d6e9dbb9723a5d989ac9f9e0fcd4afefef6
greg long <greggolong@gmail.com>
2021-04-28 23:04:38 -0500

    update outpout

--- output.tf
+++ output.tf
@@ -8,3 +8,8 @@ output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.app_server.public_ip
 }
+
+output "all" {
+  value = aws_instance.app_server
+
+}
